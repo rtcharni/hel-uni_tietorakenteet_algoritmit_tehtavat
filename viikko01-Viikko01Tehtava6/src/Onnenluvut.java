b6f7891b3db9d459b2d34d4363cf52c2e5d4c63a
elama09 <roman.tcharni@gmail.com>
2019-09-01 15:19:35 +0300

    Version 2

--- viikko01-Viikko01Tehtava6/src/Onnenluvut.java
+++ viikko01-Viikko01Tehtava6/src/Onnenluvut.java
@@ -1,17 +1,52 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Stream;
+import java.util.*;
 
 public class Onnenluvut {
     int laske(int a, int b) {
         int total = 0;
-        int length = String.valueOf(b).length();
+        int maxLengthInt = String.valueOf(b).length();
+        int minLengthInt = String.valueOf(a).length();
+
+        for (int i = minLengthInt; i <= maxLengthInt; i++) {
+            int added = 0;
+            int maxPosibilityCount = ((int) Math.pow(2, i));
+            for (int j = Integer.parseInt("3".repeat(i)); j <= Integer.parseInt("7".repeat(i)); j++) {
+
+                if ((String.valueOf(j).toCharArray()[0] != '3' && String.valueOf(j).toCharArray()[0] != '7') || j < a || j > b) {
+                    continue;
+                } else if (String.valueOf(j).chars().mapToObj(z -> (char) z).allMatch(x -> x == '3' || x == '7')) {
+                    total++;
+                    added++;
+
+                    if (added == maxPosibilityCount) {
+                        break;
+                    }
+                }
+
+            }
+        }
+        return total;
+    }
+
+    int firstWay(int a, int b) {
+        long startTime = System.nanoTime();
+
+        int total = 0;
+        // int length = String.valueOf(b).length();
         // Integer.parseInt("7".repeat(length))
-        for (int i = a; i < b; i++) {
-            if (String.valueOf(i).chars().mapToObj(z -> (char)z).allMatch(x -> x == '3' || x == '7')) {
+        for (int i = a; i <= b; i++) {
+            if (String.valueOf(i).toCharArray()[0] != '3' && String.valueOf(i).toCharArray()[0] != '7') {
+                continue;
+            } else if (String.valueOf(i).chars().mapToObj(z -> (char) z).allMatch(x -> x == '3' || x == '7')) {
                 total++;
             }
         }
-        
+        long endTime = System.nanoTime();
+        long timeElapsed = endTime - startTime;
+        System.out.println("Execution time in milliseconds : " +
+                timeElapsed / 1000000);
         return total;
     }
 }
