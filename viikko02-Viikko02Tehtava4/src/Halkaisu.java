18f8e6eb9aa1cc4a6bed7ea36da30024a3a48ac1
elama09 <roman.tcharni@gmail.com>
2019-09-07 17:07:47 +0300

    2.4 finished, now O(n)

--- viikko02-Viikko02Tehtava4/src/Halkaisu.java
+++ viikko02-Viikko02Tehtava4/src/Halkaisu.java
@@ -1,6 +1,26 @@
 public class Halkaisu {
     int laske(int[] t) {
         int totalVariation = 0;
+        int left = t[0];
+        int right = 0;
+
+        for (int i = 1; i < t.length; i++) {
+            right += t[i];
+        }
+
+        for (int i = 1; i < t.length; i++) {
+            if (left == right) {
+                totalVariation++;
+            }
+            left += t[i];
+            right -= t[i];
+        }
+
+        return totalVariation;
+    }
+
+    int firstWay(int[] t) {
+        int totalVariation = 0;
 
         int left = t[0];
 
