9b2a2bfafeb8b3e616b7f42416917ed21d1a5b64
elama09 <roman.tcharni@gmail.com>
2019-09-07 20:10:54 +0300

    wip 2.5

--- viikko02-Viikko02Tehtava5/src/Kierrokset.java
+++ viikko02-Viikko02Tehtava5/src/Kierrokset.java
@@ -1,4 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Kierrokset {
+    int lol;
+
     int laske(int[] t) {
+        List list = Arrays.stream(t)
+                .boxed()
+                .collect(Collectors.toList());
+
+
+        recursion(list, 0, 0, 1);
+        return this.lol;
+
+
+    }
+
+    void recursion(List t, int total, int startIndex, int startFind) {
+        int startnewFind = startFind;
+        int totalnew = total + 1;
+        int biggest = 0;
+        List newList = new ArrayList();
+
+        for (int i = startIndex; i < t.size(); i++) {
+            if ((int)t.get(i) == startnewFind) {
+                startnewFind++;
+            } else {
+                newList.add(t.get(i));
+            }
+
+            if ((Integer) t.get(i) > biggest) {
+                biggest = ((Integer) t.get(i)).intValue();
+            }
+        }
+
+        if (startnewFind > biggest) {
+            this.lol = totalnew;
+            return;
+        }
+
+        recursion(newList, totalnew, 0, startnewFind);
+    }
+
+    int firstWay(int[] t) {
+        int total = 1;
+
+        for (int i = 1; i < t.length; i++) {
+            if (t[i] < t[i - 1]) {
+                total++;
+            }
+        }
+
+        return total;
     }
 }
