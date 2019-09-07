b80926f25954f754646c48acee437cb0693aaeef
elama09 <roman.tcharni@gmail.com>
2019-09-07 17:00:45 +0300

    2.4 works, but not fast enough now O(n2)

--- viikko02-Viikko02Tehtava4/src/Halkaisu.java
+++ viikko02-Viikko02Tehtava4/src/Halkaisu.java
@@ -1,4 +1,22 @@
 public class Halkaisu {
     int laske(int[] t) {
+        int totalVariation = 0;
+
+        int left = t[0];
+
+        for (int i = 1; i < t.length; i++) {
+
+            int right = 0;
+            for (int j = i; j < t.length; j++) {
+                right += t[j];
+            }
+
+            if (left == right) {
+                totalVariation++;
+            }
+            left += t[i];
+
+        }
+        return totalVariation;
     }
 }
