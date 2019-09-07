c530c6c365bd6795bbe93a88bfa7a1943e131d3b
elama09 <roman.tcharni@gmail.com>
2019-09-07 16:47:24 +0300

    Finished 2.3 works

--- viikko02-Viikko02Tehtava3/src/Muutokset.java
+++ viikko02-Viikko02Tehtava3/src/Muutokset.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Muutokset {
     int laske(int[] t) {
+        int totalMoves = 0;
+        List list = new ArrayList();
+        //list.add(t[0]);
+
+        for (int i = 1; i < t.length; i++) {
+            //list.add(t[i]);
+
+            if (t[i - 1] == t[i] && i != t.length - 1) {
+                if (t[i - 1] > t[i + 1]) {
+                    t[i] = t[i - 1] + 1;
+                    totalMoves++;
+                } else if (t[i - 1] < t[i + 1]) {
+                    t[i] = t[i + 1] + 1;
+                    totalMoves++;
+                } else {
+                    t[i] = t[i - 1] + 1;
+                    totalMoves++;
+                }
+            } else if (i == t.length - 1 && t[i - 1] == t[i]) {
+                t[i] = t[i - 1] + 1;
+                totalMoves++;
+            }
+        }
+        return totalMoves;
     }
 }
