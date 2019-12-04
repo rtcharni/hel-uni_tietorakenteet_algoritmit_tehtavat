721c622b667df6da96b91d06fbc7e46d80b11235
elama09 <roman.tcharni@gmail.com>
2019-12-04 08:04:37 +0200

    12.5 finished

--- viikko12-Viikko12Tehtava5/src/Suunnat.java
+++ viikko12-Viikko12Tehtava5/src/Suunnat.java
@@ -1,30 +1,23 @@
 import java.util.*;
 
 public class Suunnat {
-    ArrayList<Kaari> lopullisetKaaret;
     ArrayList<Kaari> kaaret;
     ArrayList<Integer>[] verkot;
-    boolean[] vierailtu;
-    boolean[] vierailtu2;
-    ArrayList<Integer> topologinenLista;
-    ArrayList<Integer> tämäKierros;
-    boolean sykliLöytyi;
     int n;
     boolean[] oltuJo;
     boolean[][] matriisi;
     boolean lisättySamaan;
+    int[][] olemassa;
+    int[][] lisatty;
 
     public Suunnat(int n) {
+        olemassa = new int[n+1][n+1];
+        lisatty = new int[n+1][n+1];
         lisättySamaan = false;
         matriisi = new boolean[n + 1][n + 1];
         oltuJo = new boolean[n + 1];
-        sykliLöytyi = false;
         this.n = n;
-        tämäKierros = new ArrayList<>();
         kaaret = new ArrayList<>();
-        vierailtu = new boolean[n + 1];
-        vierailtu2 = new boolean[n + 1];
-        topologinenLista = new ArrayList<>();
         verkot = new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
             verkot[i] = new ArrayList<>();
@@ -32,31 +25,26 @@ public class Suunnat {
     }
 
     public void lisaaKaari(int a, int b) {
-//        if (!verkot[a].contains(b)) {
         verkot[a].add(b);
         verkot[b].add(a);
+        olemassa[a][b]++;
+        olemassa[b][a]++;
         if (a == b) {
             lisättySamaan = true;
         }
-//        }
     }
 
     public ArrayList<Kaari> muodosta() {
         if (lisättySamaan) {
             return null;
         }
-//        oltuJo[2] = true;
-//        syvyysHaku2(verkot[2], 2);
         for (int i = 1; i <= n; i++) {
             if (verkot[i].size() > 0 && oltuJo[i] == false) {
                 oltuJo[i] = true;
                 syvyysHaku2(verkot[i], i);
             }
         }
-
-//        if (sykliLöytyi == true) return null;
         return kaaret;
-
     }
 
     void syvyysHaku2(ArrayList<Integer> verkko, int verkkoNum) {
@@ -64,38 +52,28 @@ public class Suunnat {
             if (!oltuJo[k]) {
                 kaaret.add(new Kaari(verkkoNum, k));
                 matriisi[verkkoNum][k] = true;
-                verkot[k].remove(Integer.valueOf(verkkoNum));
+//                verkot[k].remove(Integer.valueOf(verkkoNum));
+                lisatty[verkkoNum][k]++ ;
+                lisatty[k][verkkoNum]++ ;
                 oltuJo[k] = true;
                 syvyysHaku2(verkot[k], k);
             } else {
+                if (olemassa[verkkoNum][k] == lisatty[verkkoNum][k]) {
+                    continue;
+                }
                 if (matriisi[verkkoNum][k]) {
                     kaaret.add(new Kaari(verkkoNum, k));
-                    verkot[k].remove(Integer.valueOf(verkkoNum));
+//                    verkot[k].remove(Integer.valueOf(verkkoNum));
+                    lisatty[verkkoNum][k]++ ;
+                    lisatty[k][verkkoNum]++ ;
                 } else {
                     kaaret.add(new Kaari(k, verkkoNum));
                     matriisi[k][verkkoNum] = true;
-                    verkot[k].remove(Integer.valueOf(verkkoNum));
+//                    verkot[k].remove(Integer.valueOf(verkkoNum));
+                    lisatty[verkkoNum][k]++ ;
+                    lisatty[k][verkkoNum]++ ;
                 }
             }
         }
-    }
-
-    void syvyysHaku(ArrayList<Integer> verkko, int verkkoNum) {
-        if (tämäKierros.contains(verkkoNum)) {
-            System.out.println("SYKLI LÖYTYI!!");
-            sykliLöytyi = true;
-            return;
-        }
-        if (vierailtu[verkkoNum] == true) {
-            return;
-        }
-        vierailtu[verkkoNum] = true;
-        tämäKierros.add(verkkoNum);
-        for (Integer k : verkko) {
-            syvyysHaku(verkot[k], k);
-
-        }
-        topologinenLista.add(verkkoNum);
-        tämäKierros.remove(verkko);
     }
 }
